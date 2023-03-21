cf467b96294786c11840b3bbc938fb72c9632d3a
Matthew James <Matthew.James@familysearch.org>
2023-03-21 08:36:31 -0600

    layout changes

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,21 @@
-const section1 = document.querySelector('.block1');
+const section1 = document.querySelector('.block1')
 const section2 = document.querySelector('.block2');
+
 addEventListener("scroll", (event) => {
-    console.log(window.scrollY)
-    if (window.scrollY >= 320) {
-        section1.style.opacity = 1;
-    }
-    if (window.scrollY >= 800) {
-        section2.style.opacity = 1;
-    }
+    listenToScroll(section1)
+    listenToScroll(section2)
 
 });
+
+function listenToScroll(element) {
+    const elementBound = element.getBoundingClientRect();
+    if (elementBound.top <= window.scrollY) {
+        animateIn(element);
+    }
+}
+
+function animateIn(element) {
+    element.style.opacity = 1;
+    element.style.transform = "translateX(0)";
+}
+
